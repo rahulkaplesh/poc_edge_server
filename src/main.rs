84ca2295eec4b78c1a194de0e5709f97dcadce66
Rahul Kaplesh <rahul.kaplesh@smithsdetection.com>
2021-11-25 18:49:33 +0530

    renaming file ...
    Signed-off-by: Rahul Kaplesh <rahul.kaplesh@smithsdetection.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod common;
-mod discovery_server;
+mod edge_server;
 
 fn main() {
     println!("Hello, world!");
-    let server = discovery_server::EdgeServer::new();
+    let server = edge_server::EdgeServer::new();
     match server {
         Ok(server) => server.run(),
         Err(err) => println!("Cannot Run edge server: {}", err),
